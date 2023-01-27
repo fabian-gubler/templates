3f13de5b2c50d20608750db14a8c1437f2e84e40
Fabian Gubler <fabian.gubler@protonmail.com>
2023-01-27 12:30:41 +0100

    manual update

--- flake.nix
+++ flake.nix
@@ -2,68 +2,19 @@
   description =
     "Ready-made templates for easily creating flake-driven environments";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
   outputs = { self, flake-utils, nixpkgs }:
     {
       templates = {
-
         data = {
           path = ./data;
-          description = "Scala development environment";
+          description = "Data Science development environment";
         };
 
-        zig = {
-          path = ./zig;
-          description = "Zig development environment";
+        rust = {
+          path = ./rust;
+          description = "Rust development environment";
         };
 
       };
-    } // flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        inherit (pkgs) mkShell writeScriptBin;
-        exec = pkg: "${pkgs.${pkg}}/bin/${pkg}";
-
-        format = writeScriptBin "format" ''
-          ${exec "nixpkgs-fmt"} **/*.nix
-        '';
-
-        dvt = writeScriptBin "dvt" ''
-          if [ -z $1 ]; then
-            echo "no template specified"
-            exit 1
-          fi
-
-          TEMPLATE=$1
-
-          ${exec "nix"} \
-            --experimental-features 'nix-command flakes' \
-            flake init \
-            --template \
-            "github:the-nix-way/dev-templates#''${TEMPLATE}"
-        '';
-
-        update = writeScriptBin "update" ''
-          for dir in `ls -d */`; do # Iterate through all the templates
-            (
-              cd $dir
-              ${exec "nix"} flake update # Update flake.lock
-              ${exec "direnv"} reload    # Make sure things work after the update
-            )
-          done
-        '';
-      in
-      {
-        devShells = { default = mkShell { buildInputs = [ format update ]; }; };
-
-        packages = rec {
-          default = dvt;
-
-          inherit dvt;
-        };
-      });
+    };
 }
