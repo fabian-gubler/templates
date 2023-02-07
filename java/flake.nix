78d1a6fc0414432deceb9cb9cadfe73758f6d3b3
Fabian Gubler <fabian.gubler@protonmail.com>
2023-02-07 12:23:16 +0100

    manual update

--- java/flake.nix
+++ java/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
   };
 
   outputs =
