9a773ae31d830e529a5ee18d7a25e1c9332b6e4b
Fabian Gubler <fabian.gubler@protonmail.com>
2023-02-18 20:33:16 +0100

    manual update

--- flake.nix
+++ flake.nix
@@ -21,5 +21,50 @@
         };
 
       };
-    };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) mkShell writeScriptBin;
+        exec = pkg: "${pkgs.${pkg}}/bin/${pkg}";
+
+        format = writeScriptBin "format" ''
+          ${exec "nixpkgs-fmt"} **/*.nix
+        '';
+
+        dvt = writeScriptBin "dvt" ''
+          if [ -z $1 ]; then
+            echo "no template specified"
+            exit 1
+          fi
+          TEMPLATE=$1
+          ${exec "nix"} \
+            --experimental-features 'nix-command flakes' \
+            flake init \
+            --template \
+            "github:the-nix-way/dev-templates#''${TEMPLATE}"
+        '';
+
+        update = writeScriptBin "update" ''
+          for dir in `ls -d */`; do # Iterate through all the templates
+            (
+              cd $dir
+              ${exec "nix"} flake update # Update flake.lock
+              ${exec "direnv"} reload    # Make sure things work after the update
+            )
+          done
+        '';
+      in
+      {
+        devShells = {
+          default = mkShell {
+            packages = [ format update ];
+          };
+        };
+
+        packages = rec {
+          default = dvt;
+
+          inherit dvt;
+        };
+      });
 }
